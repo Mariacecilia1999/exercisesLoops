9f83a3e7e16e5373a1030cde3b4223d8a8eb65fc
Mariacecilia1999 <conticecilia199@gamil.com>
2023-03-31 08:49:00 -0300

    Addign practice DOM exercise one

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 // Crear una función obtenerIndice que tome como argumento un valor cualquiera valor y un array cualquiera array y devuelva el índice del primer ítem con dicho valor en el array, o -1 si no hay ninguno.
 
 let $ = selector => document.querySelector(selector)
+let showExerciseOne = $('.containerExerciseOne')
+
 
 let obtenerIndice = (valor, array) =>{
    console.log(array.indexOf(valor))
+   let result = array.indexOf(valor)
+   showExerciseOne.innerHTML += `
+                                <h2>Valor: ${valor} <span>índice: ${result} <span></h2>
+                                 `
 }
 obtenerIndice(12, [5, 7, 12, 34, 54, 2, 12]) // 2
 obtenerIndice(83, [5, 7, 12, 34, 54, 2, 12]) // -1
