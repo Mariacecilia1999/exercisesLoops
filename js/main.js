901fe44da771e4a28f2b7a424805c3e03b5125c0
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-01 08:22:17 -0300

    Addign DOM for  exercise two

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 
 const add = (numbers) =>{
+   let containerExerciseTwo = document.querySelector('.containerExerciseTwo')
+   
    let accumulator = 0 
    numbers.forEach(number => {
-       accumulator += number
+      result= accumulator += number
    });
-   console.log(accumulator)
+   containerExerciseTwo.innerHTML+= `<p>Result = ${accumulator}<p>`
 }
 add([1,5,5])
 add([5,7,10,12,24])
